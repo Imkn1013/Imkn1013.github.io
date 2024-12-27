927cef459b4b7be49e654a5237209c8eb5a8509f
Imkn1013 <imkn.tr700602@gmail.com>
2024-12-27 03:46:19 +0000

    Update sanma.js

--- sanma.js
+++ sanma.js
@@ -1,7 +1,7 @@
 socket.emit("sanma_open");
 socket.on("sanma_start",function(data){
-  document.getElementById("menu").style=none;
-  document.getElementById("setting").style=none;
+  document.getElementById("menu").style.display="none";
+  document.getElementById("setting").style.display="none";
   var canvas = document.createElement("canvas");
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
