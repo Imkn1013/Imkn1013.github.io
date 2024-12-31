47a4d943088d9fc23221df8cddb0461d61e0c8eb
Imkn1013 <imkn.tr700602@gmail.com>
2024-12-31 09:20:51 +0000

    Update syogi.js

--- syogi.js
+++ syogi.js
@@ -1,9 +1,7 @@
-socket.on("which",function(){
-console.log(data);
+console.log("ok3");
 document.getElementById("menu").style.display="none";
 document.getElementById("setting").style.display="none";
 var canvas = document.createElement("canvas");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const ctx = canvas.getContext('2d');
-});
