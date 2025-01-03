b1df01d485dded230fdb0147480436946d65b5e4
Imkn1013 <imkn.tr700602@gmail.com>
2025-01-03 10:27:34 +0000

    Update syogi.js

--- syogi.js
+++ syogi.js
@@ -3,6 +3,7 @@ console.log(who_host);
 document.getElementById("menu").style.display="none";
 document.getElementById("setting").style.display="none";
 var canvas = document.createElement("canvas");
+document.body.appendChild(canvas);
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const ctx = canvas.getContext('2d');
