a1fa5dfc5848bbffecd8f4fc68b6b17a4f3b3301
Imkn1013 <imkn.tr700602@gmail.com>
2025-07-10 15:36:17 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Socket.IO server!');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+http.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
