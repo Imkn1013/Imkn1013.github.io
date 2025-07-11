c8a138bdd933ddc30642f362d4e4f5f8fd617e1d
Imkn1013 <imkn.tr700602@gmail.com>
2025-07-11 10:00:47 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const io = require('socket.io')(http, {
+  cors: {
+    origin: "https://imkn1013.github.io",
+    methods: ["GET", "POST"]
+  }
+});
+
 const socket = io('https://socket-server-sgat.onrender.com');
 const PORT = process.env.PORT || 3000;
 
